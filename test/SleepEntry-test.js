0c212d36dbe2d30791cf392f40d00cbf8c5d4e9e
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-04-02 19:07:05 -0600

    Add SleepEntry-test hours slept

--- test/SleepEntry-test.js
+++ test/SleepEntry-test.js
@@ -37,5 +37,11 @@ describe('Sleep', function() {
       expect(user3.date).to.equal("2019/06/17");
   });
 
+  it("should store a User\'s hours slept", function() {
+      expect(user1.hoursSlept).to.equal(6.1);
+      expect(user2.hoursSlept).to.equal(7.5);
+      expect(user3.hoursSlept).to.equal(5.3);
+  });
+
 
 });
