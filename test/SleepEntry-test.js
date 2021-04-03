da3722f048c6b802a9d1d19cc18e0b5393ce74f8
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-04-02 19:04:34 -0600

    Add SleepEntry-test beforeEach setup

--- test/SleepEntry-test.js
+++ test/SleepEntry-test.js
@@ -5,3 +5,15 @@ const UserRepository = require('../src/UserRepository');
 const userData = require('./test-data/user-data');
 const SleepEntry = require('../src/SleepEntry');
 const sleepData = require('./test-data/sleep-data');
+
+
+describe('Sleep', function() {
+
+  beforeEach(() => {
+    user1 = new SleepEntry(sleepData[0]);
+    user2 = new SleepEntry(sleepData[4]);
+    user3 = new SleepEntry(sleepData[8]);
+    });
+
+
+});
