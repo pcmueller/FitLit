5e66c9412c4279acbf5a991244f883d0d7d6c6e1
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-04-02 19:03:43 -0600

    Begin SleepEntry-test setup

---
+++ test/SleepEntry-test.js
@@ -0,0 +1,7 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const UserRepository = require('../src/UserRepository');
+const userData = require('./test-data/user-data');
+const SleepEntry = require('../src/SleepEntry');
+const sleepData = require('./test-data/sleep-data');
